850fec245ddb2451f81908b6eb84b7bd0e841339
Florian Winkler <f.winkler@fz-juelich.de>
2019-11-19 15:25:17 +0100

    Increased Version number to 1.1

--- printNumbers/parameters.py
+++ printNumbers/parameters.py
@@ -23,7 +23,7 @@
 # Class to process and store program parameters.
 #
 
-CONST_VERSION = 'V1.0'
+CONST_VERSION = 'V1.1'
 CONST_VERSION_STRING = '+ + PrintNumbers ' + CONST_VERSION + ' (Software Development in Science 2017) + +'
 CONST_DEF_OPERAND_VAL = 10
 CONST_MAX_OPERAND_VAL = 20
